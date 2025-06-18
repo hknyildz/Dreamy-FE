d394d0e0146aba617c047d58fcd88238a2ec808e
hknyildz <ahmethakanyildiz9@gmail.com>
2025-06-18 17:05:27 +0300

    Init commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const profile = () => {
+  return (
+    <View>
+      <Text>profile</Text>
+    </View>
+  );
+};
+
+export default profile
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const saved = () => {
+  return (
+    <View>
+      <Text>saved</Text>
+    </View>
+  );
+};
+
+export default saved
+const styles = StyleSheet.create({})
